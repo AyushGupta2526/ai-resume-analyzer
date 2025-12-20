bd8c3e4b83b7cd0ed89ff0462e57c55e0921fc32
Ayush Gupta <guptaayush2526@gmail.com>
2025-12-20 18:58:09 +0530

    Fetch real resumes on the homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,9 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "constants/index";
 import ResumeCard from "~/components/ResumeCard";
 import { usePuterStore } from "~/lib/puter";
-import { useNavigate } from "react-router";
-import { use, useEffect, useState } from "react";
-import resume from "./resume";
+import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,24 +13,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-    const { auth, fs } = usePuterStore();
+    const { auth, kv } = usePuterStore();
     const navigate = useNavigate();
-    const [resumeUrl, setResumeUrl] = useState('');
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResumes, setLoadingResumes] = useState(false);
 
     useEffect(() => {
-        if(!auth.isAuthenticated) navigate('/auth?next=/');
-    }, [auth.isAuthenticated]);
+      if (!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated]}
 
     useEffect(() => {
-      const loadResume = async () => {
-        const blob = await fs.read(resume.imagePath);
-        if(!blob) return;
-        let url = URL.createObjectURL(blob);
-        setResumeUrl(url);
+        const loadResumes = async () => {
+            setLoadingResumes(true);
+
+      const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+      const parsedResumes = resumes?.map((resume) => (
+          JSON.parse(resume.value) as Resume
+      ))
+
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
       }
 
-      loadResume
-    }, [resume.imagePath]);
+      loadResume();
+    }, []);
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
@@ -40,17 +45,32 @@ export default function Home() {
     <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
+        {!loadingResumes && resumes?.length === 0 ? (
         <h2>Review your submissions and check AI-powered feedback.</h2>
+        ) : (
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+        )}
       </div>
+        {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+      )}
+
     
-    
-      {resumes.length > 0 && (
+      {!loadingResumes && resumes.length > 0 && (
         <div className="resumes-section">
           {resumes.map((resume) => (
             <ResumeCard key={resume.id} resume={resume} />
           ))}
         </div>
       )}
+         {!loadingResumes && resumes?.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
+          )}
     </section>
   </main>
 }
